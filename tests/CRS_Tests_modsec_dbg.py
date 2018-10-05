2098de1594186ee1d42530fc42fb81c3c3e98a7b
Alon Pelled <alpelled@microsoft.com>
2018-10-05 16:29:17 -0700

    remove debug info

--- tests/CRS_Tests_modsec_dbg.py
+++ tests/CRS_Tests_modsec_dbg.py
@@ -9,10 +9,8 @@ import config
 import time
 
 def test_crs(ruleset, test, logchecker_obj):
-    #configure modsec audit loogging & need to make sure paranoia is set to 4?
-    # read the modsec conf - modsecurity.conf - set the following:
-    # SecDebugLog E:\ModSecLogs\modsec_debug.log
-    # SecDebugLogLevel 3
+    # the configuration to this test is done by appveyor.yml,
+    # the config file points to the modsec conf and sets the regex parsing
 
     runner = testrunner.TestRunner()
     for stage in test.stages:
@@ -40,7 +38,6 @@ class FooLogChecker(logchecker.LogChecker):
                 sys.exit("Please make sure that SecDebugLog & SecDebugLogLevel in {}".format(config.modsecurity_conf_location))
 
     def reverse_readline(self, filename):
-        print(filename)
         with open(filename, 'r') as f:
             f.seek(0, os.SEEK_END)
             position = f.tell()
@@ -49,15 +46,11 @@ class FooLogChecker(logchecker.LogChecker):
                 f.seek(position)
                 next_char = f.read(1)
                 if next_char == "\n":
-                    print("*********************************************************************************")
-                    print("alon1: {}".format(line[::-1]))
                     yield line[::-1]
                     line = ''
                 else:
                     line += next_char
                 position -= 1
-            print("*********************************************************************************")
-            print("alon2: {}".format(line[::-1]))
             yield line[::-1]    
 
     def get_logs(self):
@@ -65,18 +58,15 @@ class FooLogChecker(logchecker.LogChecker):
         log_date_format = config.log_date_format
         pattern = re.compile(r'%s' % log_date_regex)
         our_logs = []
-        print("reading the debug file: {}".format(self.log_file))
         for lline in self.reverse_readline(self.log_file):
             # Extract dates from each line
             match = re.match(pattern,lline)
-            print ("alon, match: {}".format(match))
             if match:
                 log_date = match.group(1)
                 print("alon, log_data: {}".format(log_date))
                 # Convert our date
                 log_date = datetime.datetime.strptime(log_date[:-1], log_date_format)
                 ftw_start = self.start
-                print("alon, log_data / ftw_start: {}".format(log_date, ftw_start))
                 # if data is bigger than start add it, otherwise abort
                 if log_date >= ftw_start.replace(microsecond=0):
                     our_logs.append(lline)
